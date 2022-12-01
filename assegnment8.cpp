8b2a22fa20b95708fe8d12c2a3a4f2694312dee4
Saidkamolkhon <118038945+Saidkamolkhon@users.noreply.github.com>
2022-12-01 15:57:50 +0500

    Update assegnment8.cpp

--- assegnment8.cpp
+++ assegnment8.cpp
@@ -13,3 +13,45 @@ int main()
     fahrenheit_celcius(67);
     return 0;
 }
+
+//                     PROBLEM2
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<int> myVector{ 6,4,5,3,3,2,4,0 };
+	for (int i = 0; i < myVector.size(); ++i) {
+		if (myVector[i] % 2 == 0) {
+			myVector[i] = 2;
+		}
+		else {
+			myVector[i] = 1;
+		}
+	}
+
+	for (int i = 0; i < myVector.size(); ++i) {
+		cout << myVector[i] << " ";
+	}
+
+	return 0;
+}
+
+//                    PROBLEM3
+#include <iostream>
+using namespace std;
+
+
+int func(int x) {
+	int y = 7 * x - 5;
+	cout << y << endl;
+
+	return 0;
+}
+
+int main() {
+	func(7);
+	func(67);
+
+	return 0;
+}
