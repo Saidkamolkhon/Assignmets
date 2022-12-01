0480aac1e633dd668c4b52ef7211b1af3133d3d5
Saidkamolkhon <118038945+Saidkamolkhon@users.noreply.github.com>
2022-12-01 22:40:31 +0500

    Update assegnment8.cpp

--- assegnment8.cpp
+++ assegnment8.cpp
@@ -55,3 +55,59 @@ int main() {
 
 	return 0;
 }
+
+
+//                    	PROBLEM4
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int num = 0, ctr = 0;
+    cin >> num;
+    int arr[num], *arrPtr = arr;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> *(arrPtr+i);
+    }
+    sort(arr, arr + num);
+    for (int i = 0; i < num; i++)
+    {
+        if (*(arrPtr + i) == *(arrPtr + i + 1))
+        {
+            ctr++;
+        }
+    }
+    cout << ctr << endl;
+}
+//                    PROBLEM5
+#include <iostream>
+using namespace std;
+int max_element(int *p1, int n1)
+{
+    for (int i = 0; i < n1; i++)
+    {
+        cin >> *(p1 + i);
+        if (*p1 < *(p1 + i))
+        {
+            *p1 = *(p1 + i);
+        }
+    }
+    return *p1;
+}
+
+int main()
+{
+    int n1, n2;
+    cin >> n1;
+    int arr1[n1];
+    int *p1 = arr1;
+    max_element(p1, n1);
+    cin >> n2;
+    int arr2[n2];
+    int *p2 = arr2;
+    max_element(p2, n2);
+    cout << *p1 * (*p2) << "\n";
+    // cout<<(*p2)*(*p1);
+}
